ffa3b3e11d7b6330bbd659501a2e04b64056a258
TheJecksMan <themarvindit@gmail.com>
2021-04-28 08:18:29 +0300

    add splashScreen

--- app/src/main/java/com/example/smartnotebook/MainActivity.java
+++ app/src/main/java/com/example/smartnotebook/MainActivity.java
@@ -4,19 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 
 public class MainActivity extends AppCompatActivity  {
+
+    private ProgressBar mProgress;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImageView = (ImageView)  findViewById(R.id.imageViewSplashScreen);
+        mProgress = (ProgressBar) findViewById(R.id.progressBarLoad);
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                StartApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress = 0; progress < 100; progress += 5) {
+            try {
+                Thread.sleep(100);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void StartApp(){
         Intent mainIntent = new Intent(this, MainActivityMainMenu.class);
         startActivity(mainIntent);
         finish();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mImageView.setImageDrawable(null);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Runtime.getRuntime().gc();
+    }
 }
