e524cbc757bd92b503eca3b1f304b1673de892af
TheJecksMan <themarvindit@gmail.com>
2021-07-09 09:17:23 +0300

    update libraries

--- app/src/main/java/com/panabey/smartnotebook/CreateList.java
+++ app/src/main/java/com/panabey/smartnotebook/CreateList.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
--- app/src/main/java/com/panabey/smartnotebook/MainMenu.java
+++ app/src/main/java/com/panabey/smartnotebook/MainMenu.java
@@ -9,7 +9,6 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -39,7 +38,7 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         BottomNavigationMenu = findViewById(R.id.BottomNavigationMenu);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentPager, new notes_fg()).commit();
 
-        BottomNavigationMenu.setOnNavigationItemSelectedListener(navListener);
+        BottomNavigationMenu.setOnItemSelectedListener(navListener);
 
         //----------------------------NavigationView-------------------------------------//
         navigationView.bringToFront();
@@ -97,32 +96,28 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
     }
 
     //---------------------------------------Нижняя панель-----------------------------------//
-    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @SuppressLint("NonConstantResourceId")
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    private final BottomNavigationView.OnItemSelectedListener navListener = item -> {
 
-            Fragment selectedFragment = null;
-            switch (item.getItemId()) {
-                case R.id.ItemNotes:
-                    selectedFragment = new notes_fg();
-                    break;
-                case R.id.ItemReminders:
-                    selectedFragment = new list_fg();
-                    break;
-                case R.id.ItemFolders:
-                    selectedFragment = new folders_fg();
-                    break;
-                case R.id.ItemAccount:
-                        selectedFragment = new accountLogin_fg();
-                    break;
-            }
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragmentPager, selectedFragment)
-                    .commit();
-            return true;
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.ItemNotes:
+                selectedFragment = new notes_fg();
+                break;
+            case R.id.ItemReminders:
+                selectedFragment = new list_fg();
+                break;
+            case R.id.ItemFolders:
+                selectedFragment = new folders_fg();
+                break;
+            case R.id.ItemAccount:
+                    selectedFragment = new accountLogin_fg();
+                break;
         }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentPager, selectedFragment)
+                .commit();
+        return true;
     };
 
 }
