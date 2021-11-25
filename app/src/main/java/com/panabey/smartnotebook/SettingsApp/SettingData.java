06277d02ce4e8d4ce374fe09ba76fdfb0dcbd27c
TheJecksMan <themarvindit@gmail.com>
2021-11-25 10:44:40 +0300

    info size database

---
+++ app/src/main/java/com/panabey/smartnotebook/SettingsApp/SettingData.java
@@ -0,0 +1,41 @@
+package com.panabey.smartnotebook.SettingsApp;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.panabey.smartnotebook.Database.SQLiteHelperKotlin;
+import com.panabey.smartnotebook.R;
+
+public class SettingData extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.setting_storage);
+        InfoSize();
+    }
+
+    private void InfoSize(){
+        Preference preferenceSizeDatabase = findPreference("key_sizeDatabase");
+
+        SQLiteHelperKotlin sqLiteHelperKotlin = new SQLiteHelperKotlin(getContext());
+        SQLiteDatabase database = sqLiteHelperKotlin.getReadableDatabase();
+
+        final String sqlSizeDatabase = "SELECT page_count * page_size as " +
+                "size FROM pragma_page_count(), pragma_page_size();";
+
+        Cursor cursor = database.rawQuery(sqlSizeDatabase, null);
+        try {
+            cursor.moveToFirst();
+            preferenceSizeDatabase.setSummary("Фактический размер: " +
+                    cursor.getInt(cursor.getColumnIndex("size"))/1024 + "Мб");
+        }
+        finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+}
--- app/src/main/java/com/panabey/smartnotebook/SettingsApp/SettingsFragmentAbout.java
+++ app/src/main/java/com/panabey/smartnotebook/SettingsApp/SettingsFragmentAbout.java
@@ -1,7 +1,11 @@
 package com.panabey.smartnotebook.SettingsApp;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.panabey.smartnotebook.R;
@@ -12,5 +16,16 @@ public class SettingsFragmentAbout extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.setting_about, rootKey);
+
+        Preference preferenceVersionApp = findPreference("key_preference_VersionApp");
+
+        Context context = getContext();
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            preferenceVersionApp.setSummary(getResources().getString(R.string.versionApp) + " " + pInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
